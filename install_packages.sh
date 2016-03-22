c69a382c9bd1b60db12415bdfc1590f95733f3dd
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-22 22:15:37 +0100

    Works

--- install_packages.sh
+++ install_packages.sh
@@ -3,3 +3,6 @@ sudo apt-get install gimp
 sudo apt-get install libmpfr-dev
 sudo apt-get install libmpc-dev
 sudo apt-get install texinfo
+sudo apt-get install libfreeimage-dev
+sudo apt-get install libusb-dev
+sudo apt-get install libusb-1.0-0-dev
