31791c7b8cf9df6bdeb33bd5453c5b6f43bc54ab
richelbilderbeek <richel@richelbilderbeek.nl>
2017-10-01 11:02:57 +0200

    Do not switch branch

--- install_libnds.sh
+++ install_libnds.sh
@@ -2,6 +2,6 @@
 cd ..
 git clone https://github.com/devkitPro/buildscripts
 cd buildscripts
-git checkout v20151213
+#git checkout v20151213
 export BUILD_DKPRO_PACKAGE=1; ./select_toolchain.sh 
 export BUILD_DKPRO_PACKAGE=1; export BUILD_DKPRO_INSTALLDIR=/opt/devkitpro; export BUILD_DKPRO_AUTOMATED=1; ./build-devkit.sh > /dev/null
