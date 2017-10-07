2518fee6bbc2699ef7c65e1e562473952d66b61e
richelbilderbeek <richel@richelbilderbeek.nl>
2017-10-07 19:55:46 +0200

    Removed old attempts

--- install_packages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get install gimp
-sudo apt-get install libmpfr-dev
-sudo apt-get install libmpc-dev
-sudo apt-get install texinfo
-sudo apt-get install libfreeimage-dev
-sudo apt-get install libusb-dev
-sudo apt-get install libusb-1.0-0-dev
